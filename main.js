5b480a6f18c8c0ee97c8708308c4dc8f2f6146bd
Neeraj Yadav <ny29599@gmail.com>
2021-04-24 12:41:10 +0530

    check dev/prod and platform, default behavior of macOS add

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const { app, BrowserWindow } = require('electron');
+//set ENV
+process.env.NODE_ENV = 'development';
+//check desired requirement
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = process.platform === 'darwin' ? true : false;
+
 let mainWindow;
 const createMainWindow = () => {
   mainWindow = new BrowserWindow({
@@ -7,8 +13,22 @@ const createMainWindow = () => {
     height: 600,
     icon: `${__dirname}/assets/icons/Icon_256x256.png`,
     // icon option is for windows only user.
+    resizable: isDev,
   })
   mainWindow.loadFile('./app/index.html')
 }
 
 app.on('ready', createMainWindow)
+
+// default behaviour of macOS
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow()
+  }
+})
