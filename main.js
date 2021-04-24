a4f45bb7414ba86369c92e1bed7531131142fb69
Neeraj Yadav <ny29599@gmail.com>
2021-04-24 12:21:03 +0530

    basic file setup like assets

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const { app, BrowserWindow } = require('electron');
-
+let mainWindow;
 const createMainWindow = () => {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: 'ImageShrink',
     width: 500,
-    height: 600
+    height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    // icon option is for windows only user.
   })
+  mainWindow.loadFile('./app/index.html')
 }
 
 app.on('ready', createMainWindow)
