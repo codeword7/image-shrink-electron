77b1690cc52ae822a77c1195e42cfc62caef8f4b
Neeraj Yadav <ny29599@gmail.com>
2021-04-25 19:29:34 +0530

    create custom about menu

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu, globalShortcut } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 //set ENV
 process.env.NODE_ENV = 'development';
 //check desired requirement
@@ -6,6 +6,8 @@ const isDev = process.env.NODE_ENV !== 'production' ? true : false;
 const isMac = process.platform === 'darwin' ? true : false;
 
 let mainWindow;
+let aboutWindow;
+// create main window
 const createMainWindow = () => {
   mainWindow = new BrowserWindow({
     title: 'ImageShrink',
@@ -18,6 +20,19 @@ const createMainWindow = () => {
   })
   mainWindow.loadFile('./app/index.html')
 }
+// create about window
+const createAboutWindow = () => {
+  aboutWindow = new BrowserWindow({
+    title: 'About ImageShrink',
+    width: 300,
+    height: 300,
+    // icon option is for windows only user.
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: false,
+    backgroundColor: 'white'
+  })
+  aboutWindow.loadFile('./app/about.html')
+}
 
 app.on('ready', () => {
 
@@ -27,19 +42,33 @@ app.on('ready', () => {
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
 
-  // add global shorcut
-  globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
-  globalShortcut.register(isMac ? 'Cmd+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools())
   // remove garbage
   mainWindow.on('closed', () => mainWindow = null)
 })
 
 // create menu for app
 const menu = [
-  ...(isMac ? [{ role: 'appMenu' }] : []),
+
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [{
+      label: 'About',
+      click: createAboutWindow
+    }]
+  }] : []),
+
   {
     role: 'fileMenu',
   },
+  // create about menu for windows OS
+  ...(!isMac ? [{
+    label: 'Help',
+    submenu: [{
+      label: 'About',
+      click: createAboutWindow
+    }]
+  }] : []),
+
   ...(isDev
     ? [
       {
